a3adfaacc6c5346bf59910cd9f05f1ab5a269af7
Qunthunnan <bredtv6@gmail.com>
2024-02-07 16:23:35 +0200

    modals animations started

--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -27,9 +27,23 @@ export class Modals {
 
     showModal(modalClass) {
         const modalElement = document.querySelector(`.${modalClass}`);
-
+        const modalContent = document.querySelector(`.${modalClass} .${this.contentSubclass}`);
+        
+        modalContent.style.cssText += `top: -22%; transform: translateX(-50%) scale(0.1); opacity: 0, border-radius: 100px;`;
         modalElement.classList.add(this.activityClass);
-    
+        modalContent.animate([
+            {transform: 'translateX(-50%) scale(0.1)',
+             top: '-22%', 
+             opacity: 0,
+            'border-radius': '100px',},
+            {transform: 'translateX(-50%) scale(1)',
+             top:'10%',
+             opacity: 1,
+             'border-radius': '20px',}
+        ], {duration: 2000, iterations: 1, fill: 'forwards'});
+
+        console.log('test');
+        
         const close = (e) => {
             if(e.target.matches(`.${this.closeBtnClass}`) || !e.target.matches(`.${this.contentSubclass}`) && !e.target.closest(`.${this.contentSubclass}`)) {
                 modalElement.classList.remove(this.activityClass);
